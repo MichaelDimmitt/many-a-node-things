9a3b80669593d5b3d1a7c79a019620e26b4402d7
Michael Dimmitt <michaelgdimmitt@gmail.com>
2018-09-19 19:59:19 -0400

    simplify logic

--- humanMessenger.js
+++ humanMessenger.js
@@ -1,27 +1,16 @@
 const { executeCurryAndSay } = require('./evalCreateCurry.js')
 
+const sayName = (evalString) => executeCurryAndSay(trimTheFat(evalString))
 const trimTheFat  = (evalString) => evalString.replace(/\((?:[()]{2})*\)/g, "")
 
-async function sayName(evalString){
-  evalString= await trimTheFat(evalString)
-  return await executeCurryAndSay(evalString) 
-}
-
 const print = async () => {
-    
-console.log(`
-the following commands work: 
-"say('hello', 'bob')('my')('name')('is')('Arundhati')('have', 'a', 'nice', 'day')('jumbo', 'shrimp')"
-output: ${await sayName("say('hello', 'bob')('my')('name')('is')('Arundhati')('have', 'a', 'nice', 'day')('jumbo', 'shrimp')")}
+  console.log(`
+  the following commands work:
+  ${await sayName("say('hello', 'bob')('my')('name')('is')('Arundhati')('have', 'a', 'nice', 'day')('jumbo', 'shrimp')")}
+  ${await sayName("say('frog', 'bob')()()()('my')('blah')")}
 
-"say('frog', 'bob')()()()('my')('blah')"
-output: ${await sayName("say('frog', 'bob')()()()('my')('blah')")}  
-
-the following commands do not work:
-sayName("('frog', 'bob')()()()('my')('blah')()()")
-output: ${await sayName("say('frog', 'bob')()()()('my')('blah')()()")}
-`)
-
+  the following commands do not work:
+  ${await sayName("say('frog', 'bob')()()()('my')('blah')()()()()")}
+  `)
 };
 print()
-
