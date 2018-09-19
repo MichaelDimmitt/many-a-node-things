93dd186b8c4e346a1dc8dbfe109e67d49d72720b
Michael Dimmitt <michaelgdimmitt@gmail.com>
2018-09-19 14:07:05 -0400

    Variadic Curry from article https://hackernoon.com/currying-in-js-d9ddc64f162e

---
+++ curry.js
@@ -0,0 +1,7 @@
+function curry(fn) {
+  return (...args) => {
+    if (args.length >= fn.length) { return fn(...args); }
+    return curry(fn.bind(null, ...args));
+  };
+}
+module.exports = { curry }
